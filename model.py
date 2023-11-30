8f0f2fbd157a75e645f7bc8ee73d3f465ca0b980
pcktm <jakub@kopanko.com>
2023-11-30 19:53:50 +0100

    loss based on just the shift between last two frames

--- model.py
+++ model.py
@@ -4,26 +4,44 @@ from torch_geometric.nn import GraphConv
 
 
 class PoseLoss(torch.nn.Module):
-    def __init__(self, alpha=200):
+    def __init__(self, batch_size, graph_length, alpha=200):
         super(PoseLoss, self).__init__()
         self.alpha = alpha
+        self.batch_size = batch_size
+        self.graph_length = graph_length
 
-    def forward(self, pred, target):
+    def forward(self, pred: torch.Tensor, target: torch.Tensor):
         # loss(X) = ||pˆ− p||2 + α||qˆ− q||2
         # pˆ, qˆ - predicted position and orientation
         # p, q - ground truth position and orientation
         # alpha - hyperparameter to weight the orientation loss
 
-        # pred is a [x, y, z, w, x, y, z] tensor
-        pred_position = pred[:, :3]
-        pred_orientation = pred[:, 3:]
-        target_position = target[:, :3]
-        target_orientation = target[:, 3:]
+        # take the last and second to last elements of the graph, remember that
+        # input pred and target are in the form [x, y, z, w, x, y, z] tensor x graph_length x batch_size
+        # torch.Size([512, 7]) for graph_length=8 and batch_size=64
 
-        position_loss = F.mse_loss(pred_position, target_position)
-        orientation_loss = F.mse_loss(pred_orientation, target_orientation)
+        pred = pred.view(-1, self.graph_length, 7)
+        target = target.view(-1, self.graph_length, 7)
 
-        return position_loss + orientation_loss * self.alpha
+        # Get the last and second-to-last nodes for each element in the batch
+        last_pred = pred[:, -1, :]
+        second_last_pred = pred[:, -2, :]
+        last_target = target[:, -1, :]
+        second_last_target = target[:, -2, :]
+
+        # Calculate shifts in position and orientation
+        shift_pred_position = last_pred[:, :3] - second_last_pred[:, :3]
+        shift_target_position = last_target[:, :3] - second_last_target[:, :3]
+        shift_pred_orientation = last_pred[:, 3:] - second_last_pred[:, 3:]
+        shift_target_orientation = last_target[:, 3:] - second_last_target[:, 3:]
+
+        # Calculate loss
+        position_loss = F.mse_loss(shift_pred_position, shift_target_position)
+        orientation_loss = F.mse_loss(shift_pred_orientation, shift_target_orientation)
+
+        # Return weighted sum of position and orientation loss
+        total_loss = position_loss + self.alpha * orientation_loss
+        return total_loss
 
 
 class PoseGNN(torch.nn.Module):
@@ -40,7 +58,7 @@ class PoseGNN(torch.nn.Module):
 
         # 4 for quaternion
         self.orientation = torch.nn.Linear(64, 4)
-        
+
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
 
